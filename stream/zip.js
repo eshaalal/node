42a241bf6e0dd1d5f5e47e0b0a62baa501b2bf59
eshaalal <eshalal888@gmail.com>
2024-02-12 10:52:49 +0530

    changes

--- stream/zip.js
+++ stream/zip.js
@@ -1,1 +1,23 @@
- 
+var zlib = require('zlib');
+var fs = require('fs');
+var gzip = zlib.createGzip();
+
+var r =fs.createReadStream('test8.txt');
+var w= fs.createWriteStream('test8.txt.gz');
+
+r.pipe(gzip).pipe(w);
+var zlib= require('zlib');
+var fs = require('fs');
+
+var brot = zlib.createBrotliCompress();
+var r = fs.createReadStream('test8.txt');
+var w = fs.createWriteStream('test8.txt.gz');
+
+r.pipe(brot).pipe(w);
+var zlib = require('zlib');
+var fs = require('fs');
+var defl = zlib.createDeflate();
+
+var r = fs.createReadStream('test8.txt');
+var w = fs.createWriteStream('test8.txt.gz');
+r.pipe(defl).pipe(w);
